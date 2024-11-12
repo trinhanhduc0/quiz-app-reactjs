4a929d659b92a6450e2124df9e0d806d022907e4
trinhanhduc0 <155358378+trinhanhduc0@users.noreply.github.com>
2024-11-12 17:43:04 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    vercel(), // Thêm plugin vercel
+  ],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
